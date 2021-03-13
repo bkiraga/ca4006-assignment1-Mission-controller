d8c72bdbe830aa464cfdcd7ebfc6f3223107f79d
kiragab2 <bartlomiej.kiraga2@mail.dcu.ie>
2021-03-13 13:48:39 +0000

    early version of components and mission

--- Component.java
+++ Component.java
@@ -1,3 +1,15 @@
+import java.util.Random;
+
 public class Component {
-    
+
+    String type;
+    int reportRate;
+    int missionSize;
+
+    public Component(String type) {
+        this.type = type;
+        Random random = new Random();
+        reportRate = random.nextInt(9) + 1;
+        missionSize = random.nextInt(9) + 1;
+    }
 }
--- Mission.java
+++ Mission.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+import java.util.Random;
 public class Mission {
-    
+
+    long startTime;
+    ArrayList<Component> componentList;
+    String destination;
+
+    public Mission(String destination) {
+        this.destination = destination;
+        long currentTime = System.currentTimeMillis();
+        Random random = new Random();
+        startTime = random.nextInt() + currentTime;
+        // componentList = new ArrayList<Component>();
+
+    }
 }
