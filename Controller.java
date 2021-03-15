71c55d0a714c078f6cf640d3d04be0b6bb489aa2
kiragab2 <bartlomiej.kiraga2@mail.dcu.ie>
2021-03-15 17:26:25 +0000

    added basic outline of controller

--- Controller.java
+++ Controller.java
@@ -1,6 +1,31 @@
 public class Controller {
+
+    public void constructMissionComponents(){
+
+    }
     
-    public void startMission(int startTime, String destination){
+    public void launchMission(int startTime, String destination){
+
+    }
+
+    public void updateMissionStage(){
+
+    }
+
+    public void checkFailures(){
+
+    }
+
+    public void sendInstructions(){
+
+    }
+
+    public void sendReports(){
         
     }
+
+    public void sendSoftwareUpdate(){
+
+    }
+
 }
--- Mission.java
+++ Mission.java
@@ -4,6 +4,7 @@ public class Mission implements Runnable{
     int startTime;
     ArrayList<Component> componentList;
     String destination;
+    String stage;
 
     public Mission(int startTime, String destination) {
         this.startTime = startTime;
